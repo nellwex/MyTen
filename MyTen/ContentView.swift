5d1005cf5f4b26d2f6c850397d18e03a25cad6f1
nellwex <nellwex@gmail.com>
2022-08-10 09:31:14 +0800

    Initial Commit

---
+++ MyTen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyTen
+//
+//  Created by 魏頤綸 on 2022/8/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyTen/MyTenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTenApp.swift
+//  MyTen
+//
+//  Created by 魏頤綸 on 2022/8/10.
+//
+
+import SwiftUI
+
+@main
+struct MyTenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
